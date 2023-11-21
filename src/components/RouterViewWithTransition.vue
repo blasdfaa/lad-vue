8fefd27d7542d373e54ef22e40a59f69f52da7e0
Vikto Zdorovcev <v.zdvacew@gmail.com>
2023-11-21 14:34:57 +0700

    reuse router view with transitions

---
+++ src/components/RouterViewWithTransition.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <Transition name="router" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </template>
+  </RouterView>
+</template>
