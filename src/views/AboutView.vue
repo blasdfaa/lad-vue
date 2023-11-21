ee27284ae7c0ff30893bd2f7c8d13e8905dc9ee8
Vikto Zdorovcev <v.zdvacew@gmail.com>
2023-11-21 14:35:08 +0700

    add page header

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,10 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+  import { NPageHeader } from 'naive-ui';
+  import { useRouter } from 'vue-router';
 
-<style></style>
+  const router = useRouter();
+</script>
+
+<template>
+  <NPageHeader title="About" @back="router.back" />
+</template>
