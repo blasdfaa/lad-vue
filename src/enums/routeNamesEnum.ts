0efaaeede311cbfa7fd35749c7b42153be5474d0
Vikto Zdorovcev <v.zdvacew@gmail.com>
2023-11-21 19:24:05 +0700

    add enum for route names

---
+++ src/enums/routeNamesEnum.ts
@@ -0,0 +1,5 @@
+export enum RouteNamesEnum {
+  HOME = 'home',
+  ABOUT = 'about',
+  SIGN_IN = 'sign-in',
+}
--- src/layouts/contants.ts
+++ src/layouts/contants.ts
@@ -1,15 +1,16 @@
 import type { MenuOption } from 'naive-ui';
 import { RenderUtils } from '~/lib/utils/render';
 import { HomeOutline } from '@vicons/ionicons5';
+import { RouteNamesEnum } from '~/enums/routeNamesEnum';
 
 export const menuOptions: MenuOption[] = [
   {
-    label: RenderUtils.renderMenuLink('Home', { name: 'home' }),
+    label: RenderUtils.renderMenuLink('Home', { name: RouteNamesEnum.HOME }),
     key: 'home',
     icon: RenderUtils.renderIcon(HomeOutline),
   },
   {
-    label: RenderUtils.renderMenuLink('About', { name: 'about' }),
+    label: RenderUtils.renderMenuLink('About', { name: RouteNamesEnum.ABOUT }),
     key: 'about',
     icon: RenderUtils.renderIcon(HomeOutline),
   },
