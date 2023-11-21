c89897f06c545e9ed95d95b6e4fc3ee136e35568
Vikto Zdorovcev <v.zdvacew@gmail.com>
2023-11-21 19:23:45 +0700

    add auth layout

---
+++ src/layouts/AppLayoutAuth.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import { NLayout, NLayoutFooter, NSpace } from 'naive-ui';
+  import { defineAsyncComponent } from 'vue';
+
+  const ThemeSwitcher = defineAsyncComponent(() => import('./components/ThemeSwitcher.vue'));
+</script>
+
+<template>
+  <NLayout>
+    <div class="content">
+      <slot />
+    </div>
+
+    <NLayoutFooter position="absolute" class="footer" bordered>
+      <NSpace align="center" justify="center">
+        <ThemeSwitcher />
+      </NSpace>
+    </NLayoutFooter>
+  </NLayout>
+</template>
+
+<style lang="css" scoped>
+  .content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+  .footer {
+    padding: 10px 0;
+  }
+</style>
